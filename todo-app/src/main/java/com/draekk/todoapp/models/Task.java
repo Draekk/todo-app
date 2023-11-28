50e2686a92059f4a7ca71cf5c0c28365b5c832bd
Draekk <rodriver1992@gmail.com>
2023-11-28 17:17:12 -0300

    mapped Task entity

--- todo-app/src/main/java/com/draekk/todoapp/models/Task.java
+++ todo-app/src/main/java/com/draekk/todoapp/models/Task.java
@@ -1,6 +1,73 @@
 
 package com.draekk.todoapp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Task {
-        
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    private boolean isCompleted;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    public Task(String description, boolean isCompleted, User user) {
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.user = user;
+    }
+
+    public Task(long id, String description, boolean isCompleted, User user) {
+        this.id = id;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
 }
--- todo-app/src/main/java/com/draekk/todoapp/models/User.java
+++ todo-app/src/main/java/com/draekk/todoapp/models/User.java
@@ -1,4 +1,3 @@
-
 package com.draekk.todoapp.models;
 
 import jakarta.persistence.Entity;
@@ -10,94 +9,95 @@ import java.util.List;
 
 @Entity
 public class User {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private long id;
-        private String name;
-        private String lastName;
-        private String email;
-        private String username;
-        private String password;
-        @OneToMany(mappedBy = "user")
-        private List<Task> taskList;
 
-        public User() {
-        }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    
+    @OneToMany(mappedBy = "user")
+    private List<Task> taskList;
 
-        public User(String name, String lastName, String email, String username, String password, List<Task> taskList) {
-                this.name = name;
-                this.lastName = lastName;
-                this.email = email;
-                this.username = username;
-                this.password = password;
-                this.taskList = taskList;
-        }
+    public User() {
+    }
 
-        public User(long id, String name, String lastName, String email, String username, String password, List<Task> taskList) {
-                this.id = id;
-                this.name = name;
-                this.lastName = lastName;
-                this.email = email;
-                this.username = username;
-                this.password = password;
-                this.taskList = taskList;
-        }
+    public User(String name, String lastName, String email, String username, String password, List<Task> taskList) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.taskList = taskList;
+    }
 
-        public long getId() {
-                return id;
-        }
+    public User(long id, String name, String lastName, String email, String username, String password, List<Task> taskList) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.taskList = taskList;
+    }
 
-        public void setId(long id) {
-                this.id = id;
-        }
+    public long getId() {
+        return id;
+    }
 
-        public String getName() {
-                return name;
-        }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-        public void setName(String name) {
-                this.name = name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getLastName() {
-                return lastName;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setLastName(String lastName) {
-                this.lastName = lastName;
-        }
+    public String getLastName() {
+        return lastName;
+    }
 
-        public String getEmail() {
-                return email;
-        }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public String getUsername() {
-                return username;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public void setUsername(String username) {
-                this.username = username;
-        }
+    public String getUsername() {
+        return username;
+    }
 
-        public String getPassword() {
-                return password;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-        public void setPassword(String password) {
-                this.password = password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public List<Task> getTaskList() {
-                return taskList;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public void setTaskList(List<Task> taskList) {
-                this.taskList = taskList;
-        }
-        
-        
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
 }
