2aa5a486ab0df488f60a7d7966686df8e6f0f1a3
Draekk <rodriver1992@gmail.com>
2023-11-29 01:44:53 -0300

    created User and Task entities

---
+++ todomaster/src/main/java/com/draekk/todomaster/models/Task.java
@@ -0,0 +1,78 @@
+
+package com.draekk.todomaster.models;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Basic
+    private String description;
+    private boolean isCompleted;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    public Task(String description, boolean isCompleted, User user) {
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.user = user;
+    }
+
+    public Task(long id, String description, boolean isCompleted, User user) {
+        this.id = id;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
---
+++ todomaster/src/main/java/com/draekk/todomaster/models/User.java
@@ -0,0 +1,115 @@
+
+package com.draekk.todomaster.models;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Basic
+    private String name;
+    private String lastName;
+    
+    @Column(unique = true)
+    private String email;
+    
+    @Basic
+    private String username;
+    private String password;
+    
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String email, String username, String password, List<Task> tasks) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.tasks = tasks;
+    }
+
+    public User(long id, String name, String lastName, String email, String username, String password, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.tasks = tasks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    
+    
+}
